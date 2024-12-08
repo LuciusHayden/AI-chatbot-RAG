b28878f35f0dcbb534be482e5c4df2db5e15c846
Lucius Hayden <luciushaydenrocks@gmail.com>
2024-12-08 15:59:29 -0800

    RAG changes

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API")
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API")
 
-print(os.getenv("OPENAI_API_KEY"))
 from langchain_openai import ChatOpenAI
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -16,8 +15,13 @@ from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_core.runnables import chain
 from langchain_core.documents import Document
 from langchain import hub
+from langchain_core.tools import tool
+from langchain_core.messages import SystemMessage, HumanMessage
 
-from langgraph.graph import START, StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.graph import START, END, StateGraph, MessagesState, StateGraph
+from langgraph.checkpoint.memory import MemorySaver
+
 
 from typing_extensions import List, TypedDict
 
@@ -44,26 +48,82 @@ print(len(doc_chunks))
 
 ids = vector_store.add_documents(doc_chunks)
 
-class State(TypedDict):
-    question: str
-    answer: str
-    context: List[Document]
+
+graph_builder = StateGraph(MessagesState)
+
+@tool(response_format="content_and_artifact")
+def retrieve(query: str):
+    """Retrieve information related to a query."""
+    retrieved_docs =  vector_store.similarity_search(query, k=2)
+    serialized = "\n\n".join((f"Source: {doc.metadata['source']} Content: {doc.page_content}") for doc in retrieved_docs)
+    return serialized, retrieved_docs
+
+def query_or_respond(state: MessagesState):
+    """Generate tool call for retrieval or respond."""
+    model_tools = model.bind_tools([retrieve])
+    response = model_tools.invoke(state['messages'])
+    return {"messages": response}
+
+tools = ToolNode([retrieve])
+
+def generate(state: MessagesState):
+    """Generate answer."""
+    recent_messages = []
+    for message in reversed(state['messages']):
+        if message.type == "tool":
+            recent_messages.append(message)
+        else:
+            break
+    tool_messages = recent_messages[::-1]
+
+    docs_content = "\n\n".join(doc.content for doc in tool_messages)
+    system_message_content = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        f"{docs_content}"
+    )
+
+    conversation_messages = [
+        message for message in state["messages"]
+        if message.type in ('human', 'system')
+        or (message.type == 'ai' and not message.tool_calls)
+    ]
+    prompt = [SystemMessage(system_message_content)] + conversation_messages
+
+    response = model.invoke(prompt)
+    return {"messages" : [response]}
+
+graph_builder.add_node(query_or_respond)
+graph_builder.add_node(tools)
+graph_builder.add_node(generate)
+
+graph_builder.set_entry_point("query_or_respond")
+graph_builder.add_conditional_edges(
+    "query_or_respond",
+    tools_condition,
+    {END: END, "tools": "tools"},
+)
+
+graph_builder.add_edge("tools", "generate")
+graph_builder.add_edge("generate", END)
+
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
 
 
-def retrieve(state: State):
-    retrieved_docs =  vector_store.similarity_search(state["question"])
-    print(retrieved_docs)
-    return {'context' : retrieved_docs}
+config = {"configurable": {"thread_id" : "abc123"}}
 
-def generate(state: State):
-    docs_content = "\n\n".join([doc.page_content for doc in state["context"]])
-    messages = prompt.invoke({"question": state["question"], "context": docs_content})
-    response = model.invoke(messages)
-    return {'answer': response.content}
 
-graph_builder = StateGraph(State).add_sequence([retrieve, generate])
-graph_builder.add_edge(START, "retrieve")
-graph = graph_builder.compile()
+prompt = "How many points do I get for a third level ascent?"
 
-response = graph.invoke({"question": "How many points do I lose for a major foul?"})
-print(response["answer"])
+
+response = graph.invoke(
+    {"messages": [{"role": "user", "content": prompt}]},
+    config=config
+)
+
+print(response)
